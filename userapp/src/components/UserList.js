13777c33fa2c993de48b2f4c01e133342daf565a
Fiona Githaiga <111144758+FionaG26@users.noreply.github.com>
2024-09-20 18:49:21 +0300

    Update UserList.js

--- userapp/src/components/UserList.js
+++ userapp/src/components/UserList.js
@@ -5,26 +5,40 @@ const UserList = () => {
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
-    const fetchUsers = async () => {
-      try {
-        const response = await axios.get('http://localhost:3000/users');
-        setUsers(response.data);
-      } catch (error) {
-        console.error('Error fetching users:', error);
-      }
-    };
-
     fetchUsers();
   }, []);
+
+  // Fetch all users
+  const fetchUsers = async () => {
+    try {
+      const response = await axios.get('http://localhost:3000/users');
+      setUsers(response.data);
+    } catch (error) {
+      console.error('Error fetching users:', error);
+    }
+  };
+
+  // Delete a user by ID
+  const deleteUser = async (userId) => {
+    try {
+      await axios.delete(`http://localhost:3000/users/${userId}`);
+      // Remove the deleted user from the state
+      setUsers(users.filter(user => user._id !== userId));
+    } catch (error) {
+      console.error('Error deleting user:', error);
+    }
+  };
 
   return (
     <ul>
       {users.map(user => (
-        <li key={user._id}>{user.name} - {user.email}</li>
+        <li key={user._id}>
+          {user.name} - {user.email}
+          <button onClick={() => deleteUser(user._id)}>Delete</button>
+        </li>
       ))}
     </ul>
   );
 };
 
 export default UserList;
-
